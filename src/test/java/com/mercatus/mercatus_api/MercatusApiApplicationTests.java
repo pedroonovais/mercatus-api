bb0fb8918bf3ce75efebb162f3be58824c013984
Pedro Novais <phenrique101007@gmail.com>
2025-04-17 16:06:13 -0300

    first commit

---
+++ src/test/java/com/mercatus/mercatus_api/MercatusApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercatus.mercatus_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercatusApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
