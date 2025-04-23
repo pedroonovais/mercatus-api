4688fe909a00302bb6a4dceefb5d7cb171e6450a
Pedro Novais <phenrique101007@gmail.com>
2025-04-23 08:43:17 -0300

    refactor specfication filters for character

--- src/main/java/com/mercatus/mercatus_api/specfication/CharacterSpecification.java
+++ src/main/java/com/mercatus/mercatus_api/specfication/CharacterSpecification.java
@@ -1,6 +1,7 @@
 package com.mercatus.mercatus_api.specfication;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import jakarta.persistence.criteria.Predicate;
@@ -8,29 +9,34 @@ import jakarta.persistence.criteria.Predicate;
 import org.springframework.data.jpa.domain.Specification;
 
 import com.mercatus.mercatus_api.model.CharacterFilter;
+import com.mercatus.mercatus_api.model.CharacterType;
 import com.mercatus.mercatus_api.model.Character;
 
 public abstract class CharacterSpecification {
 
-    public static Specification<Character> withFilters(CharacterFilter filter){
+    public static Specification<Character> withFilters(CharacterFilter filter) {
         return (root, query, cb) -> {
-            
             List<Predicate> predicates = new ArrayList<>();
 
-            if(filter.name() != null){
-                predicates.add( cb.like(
-                    cb.lower(root.get("name")), "%" + filter.name().toLowerCase() + "%"
+            if (filter.name() != null && !filter.name().isBlank()) {
+                predicates.add(cb.like(
+                    cb.lower(root.get("name")),
+                    "%" + filter.name().toLowerCase() + "%"
                 ));
             }
 
-            if(filter.characterType() != null){
-                predicates.add( cb.like(
-                    cb.lower(root.get("characterType")), "%" + filter.characterType().toLowerCase() + "%"
-                ));
+            if (filter.characterType() != null && !filter.characterType().isBlank()) {
+                List<CharacterType> matchingTypes = Arrays.stream(CharacterType.values())
+                    .filter(e -> e.name().toLowerCase().contains(filter.characterType().toLowerCase()))
+                    .toList();
+
+                if (!matchingTypes.isEmpty()) {
+                    predicates.add(root.get("characterType").in(matchingTypes));
+                }
             }
 
-            var arrayPredicates = predicates.toArray(new Predicate[0]);
-            return cb.and(arrayPredicates);
+            return cb.and(predicates.toArray(new Predicate[0]));
         };
     }
+
 }
